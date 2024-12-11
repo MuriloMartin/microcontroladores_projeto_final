cd554aba305fa958a7bbb5b05de126dcf971f2fe
Murilo Martin <murilodsmartin@gmail.com>
2024-12-11 14:18:49 -0300

    changes to player

--- src/player.py
+++ src/player.py
@@ -5,11 +5,11 @@ from serial import Serial
 import time as code_time
 
 last_serial_write_time = 0
-meu_serial = Serial(port='/dev/ttyUSB0', baudrate=9600)
+#meu_serial = Serial(port='/dev/ttyUSB0', baudrate=9600)
 
 
 # Load the vibration data (assuming you have already saved it as 'vibration_pattern.json')
-with open('data/processed/nutcracker_vibration_pattern.json', 'r') as fp:
+with open('data/processed/FMA_1_ROCK_vibration_pattern.json', 'r') as fp:
     vibration_data = json.load(fp)
 
 low_energy = np.array(vibration_data['low'])
@@ -29,19 +29,25 @@ def callback(data, frames, time, status):
     mid_intensity = mid_energy[idx]
     high_intensity = high_energy[idx]
     visualization = (
-        f"L: {'#' * 2 * int(low_intensity)}".ljust(50) +  # Bass energy with fixed width
-        f"M: {'*' * 2 * int(mid_intensity)}".ljust(50) +  # Mid energy with fixed width
-        f"H: {'-' * 2 * int(high_intensity)}".ljust(50)  # Treble energy with fixed width
+        f"L: {'#' * 1 * int(low_intensity)}".ljust(50) +  # Bass energy with fixed width
+        f"M: {'*' * 1 * int(mid_intensity)}".ljust(50) +  # Mid energy with fixed width
+        f"H: {'-' * 1 * int(high_intensity)}".ljust(50)  # Treble energy with fixed width
     )
 
-    serial_string = "bass off\n"
-    if low_intensity > int(1):
-        serial_string = f"bass\n"
+    serial_string = "bass off\noff"
+
+
+    if int(low_intensity) > 1:
+        serial_string = serial_string.replace('bass off',f"bass {str(low_intensity*10)}")
+
+    if int(high_intensity) > 1:
+        serial_string = serial_string.replace('\noff','\non')
 
     global last_serial_write_time
     if code_time.time() - last_serial_write_time >= 0.1:
-        print('------------------------ enviei info ------------------------')
-        meu_serial.write(serial_string.encode("UTF-8"))
+        #print('------------------------ enviei info ------------------------')
+        #print(serial_string)
+        #meu_serial.write(serial_string.encode("UTF-8"))
         last_serial_write_time = code_time.time()  
     
     
